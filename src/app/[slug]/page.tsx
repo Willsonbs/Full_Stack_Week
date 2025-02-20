28da2d1214f6dc4001459dd24e268b0b9c823da7
willsonbs <willsonbs@gmail.com>
2025-02-20 17:14:37 -0300

    feat: add unique constraint to restaurant slug

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,57 @@
+import { db } from "@/lib/prisma";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import ConsumptionMethodOption from "./components/consumption-method-options";
+
+interface RestaurantPageProps {
+  params: Promise<{ slug: string }>;
+}
+
+const RestaurantPage = async ({ params }: RestaurantPageProps) => {
+  const { slug } = await params;
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+  if (!restaurant) {
+    return notFound();
+  }
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      {/* Logo e título */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant.avatarImageUrl}
+          alt={restaurant.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      {/* Bem-vindo */}
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo!</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos aqui para
+          oferecer praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+      {/**/}
+      <div className="grid grid-cols-2 gap-4 pt-14">
+        <ConsumptionMethodOption
+          slug={slug}
+          option="DINE_IN"
+          buttonText="Para comer aqui"
+          imageAlt="Comer aqui"
+          imageUrl="/dine_in.png"
+        />
+        <ConsumptionMethodOption
+          slug={slug}
+          option="TAKEAWAY"
+          buttonText="Para levar"
+          imageAlt="Para levar"
+          imageUrl="/take_away.png"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const HomePage = () => {
   return (
-    <h1 className=" text-blue-600">Hello World</h1>
+    <h1 className=" text-blue-600">FSW Donalds</h1>
   )
 }
  
